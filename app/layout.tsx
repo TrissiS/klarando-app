899c8821657659c8fcf949edd011139f7af8e627
Tristan Stenger <tristanstenger@googlemail.com>
2026-05-19 21:15:04 +0200

    Set Klarando icon as global favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import CookieConsentBanner from "@/components/CookieConsentBanner";
 export const metadata: Metadata = {
   title: "Klarando",
   description: "Klarando Bestell- und Verwaltungsplattform",
+  icons: {
+    icon: [{ url: "/klarando_icon.png", type: "image/png" }],
+    shortcut: [{ url: "/klarando_icon.png", type: "image/png" }],
+    apple: [{ url: "/klarando_icon.png", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({
